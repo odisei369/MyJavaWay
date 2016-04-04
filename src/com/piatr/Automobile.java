b67abebf1a0654f5d6073dddd0903022e8203437
odisei <petrenko.ilia@gmail.com>
2016-04-04 09:16:44 +0300

    add enum

---
+++ src/com/piatr/Automobile.java
@@ -0,0 +1,14 @@
+package com.piatr;
+
+
+public class Automobile {
+    enum CarSize {SMALL, MEDIUM, LARGE}
+
+    private CarSize size;
+     public static void main(String[] args){
+         Automobile car = new Automobile();
+         car.size = CarSize.LARGE;
+         System.err.println("Size of car: " + car.size);
+         System.err.println("commit in second branch");
+     }
+}
