82888ee08f775c34aa4b0b164c82e7a5083eee14
odisei <petrenko.ilia@gmail.com>
2016-04-05 11:25:15 +0300

    Local variable test

---
+++ src/com/piatr/objectTest/variables/LocalVariable.java
@@ -0,0 +1,14 @@
+package com.piatr.objectTest.variables;
+
+public class LocalVariable{
+    public void pupAge(){
+        int age = 0;
+        age = age + 7;
+        System.out.println("Puppy age is : " + age);
+    }
+
+    public static void main(String args[]){
+        LocalVariable test = new LocalVariable();
+        test.pupAge();
+    }
+}
