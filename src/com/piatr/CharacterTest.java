f4e44718dd8c8bf2b68af1328f74fbd35e691583
odisei <petrenko.ilia@gmail.com>
2016-04-05 11:21:31 +0300

    character test

---
+++ src/com/piatr/CharacterTest.java
@@ -0,0 +1,15 @@
+package com.piatr;
+
+
+public class CharacterTest {
+    public static void main(String[] args) {
+        System.out.println("new line\n\n\naaa");
+        System.out.println("carriage return\r\r\raaa");
+        System.out.println("Formfeed\f\f\faaa");
+        System.out.println("Backspace\b\b\baaa");
+        System.out.println("Tab\t\t\taaa");
+        System.out.println("Double quote\"\"\"aaa");
+        System.out.println("Single quote\'\'\'aaa");
+        System.out.println("Backslash\\\\\\aaa");
+    }
+}
