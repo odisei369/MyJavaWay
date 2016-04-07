77149aba951283166720d66eea6c527b32de0279
odisei369 <petrenko.ilia@gmail.com>
2016-04-08 00:07:46 +0300

    add LogicalOperators Example

---
+++ src/com/piatr/BasicOperators/LogicalOperators.java
@@ -0,0 +1,16 @@
+package com.piatr.BasicOperators;
+
+
+public class LogicalOperators {
+
+    public static void main(String args[]) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a&&b));
+
+        System.out.println("a || b = " + (a||b) );
+
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
