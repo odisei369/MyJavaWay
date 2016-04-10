94829df130a0aadda0f0e0cd69926ee05cf8cf75
odisei369 <petrenko.ilia@gmail.com>
2016-04-10 12:40:36 +0300

    add WhileLoop Example

---
+++ src/com/piatr/LoopControl/WhileLoop.java
@@ -0,0 +1,15 @@
+package com.piatr.LoopControl;
+
+
+public class WhileLoop {
+
+    public static void main(String args[]) {
+        int x = 10;
+
+        while( x < 20 ) {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
